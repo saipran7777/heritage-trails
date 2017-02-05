e35f378d5f50e17fe99586762b6236fba8302623
omkarpatil18 <indian.op45@gmail.com>
2017-02-05 22:22:51 +0530

    final(hopefully)

--- app/src/main/java/in/ac/iitm/students/heritagetrails/HelpActivity.java
+++ app/src/main/java/in/ac/iitm/students/heritagetrails/HelpActivity.java
@@ -2,7 +2,6 @@ package in.ac.iitm.students.heritagetrails;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -13,7 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Toast;
+import android.widget.RelativeLayout;
 
 /**
  * Created by Nitin on 04-02-2017.
@@ -23,7 +22,7 @@ public class HelpActivity extends AppCompatActivity {
     static final int NUM_ITEMS = 3;
     private Button btnSkip;
     private Button btnNext;
-    private LinearLayout ll;
+    private RelativeLayout rl;
     private String who_called_me="blah";
 
     MyAdapter mAdapter;
@@ -39,7 +38,7 @@ public class HelpActivity extends AppCompatActivity {
             who_called_me = getIntent().getStringExtra("who_called_me");
         }
         //Toast.makeText(this, who_called_me, Toast.LENGTH_SHORT).show();
-        ll = (LinearLayout) findViewById(R.id.linear_layout);
+        rl = (RelativeLayout) findViewById(R.id.relative_layout);
         mAdapter = new MyAdapter(getSupportFragmentManager(),this);
 
         mPager = (ViewPager) findViewById(R.id.pager);
@@ -92,19 +91,19 @@ public class HelpActivity extends AppCompatActivity {
             if (position ==0) {
                 btnNext.setText(getString(R.string.next));
                 btnSkip.setVisibility(View.VISIBLE);
-                ll.setBackgroundColor(ContextCompat.getColor(HelpActivity.this,R.color.help_screen_1));
+                rl.setBackgroundColor(ContextCompat.getColor(HelpActivity.this,R.color.help_screen_1));
             }
             else if (position ==1) {
                 btnNext.setText(getString(R.string.next));
                 btnSkip.setVisibility(View.VISIBLE);
-                ll.setBackgroundColor(ContextCompat.getColor(HelpActivity.this,R.color.help_screen_2));
+                rl.setBackgroundColor(ContextCompat.getColor(HelpActivity.this,R.color.help_screen_2));
             }
             // changing the next button text 'NEXT' / 'GOT IT'
             else if (position ==2) {
                 // last page. make button text to GOT IT
                 btnNext.setText(getString(R.string.start));
                 btnSkip.setVisibility(View.GONE);
-                ll.setBackgroundColor(ContextCompat.getColor(HelpActivity.this,R.color.help_screen_3));
+                rl.setBackgroundColor(ContextCompat.getColor(HelpActivity.this,R.color.help_screen_3));
             }
         }
 
